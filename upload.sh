526178bda6fea1e41ac028115c66c1e82ed5499b
Charlie Dyson <charlie@charliedyson.net>
2019-09-18 17:36:48 +0100

    Tweak upload script

--- upload.sh
+++ upload.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -ue -o pipefail
 hugo --cleanDestinationDir
-host=107.180.50.234
+host=dukeellington.org.uk
 user=charlied@dukeellington.org.uk
 
 # TODO: This never removes anything :( Should fix this but leave TDWAW alone.
-lftp -c "open -u $user $host; mirror -R public ."
+lftp -c "open $host; mirror -R public ."
