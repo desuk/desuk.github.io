56ce974cc9c3e7dad8e45cea7a3300666d8900c9
Charlie Dyson <charlie@charliedyson.net>
2020-10-11 12:19:31 +0100

    Tweak to stop pCloud syncing my files

--- upload.sh
+++ upload.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 set -ue -o pipefail
-hugo --cleanDestinationDir
+rm -rf build
+hugo # --cleanDestinationDir
 host=dukeellington.org.uk
 user=charlied@dukeellington.org.uk
 
 # TODO: This never removes anything :( Should fix this but leave TDWAW alone.
-lftp -c "open $host; mirror -R public ."
+lftp -c "open $host; mirror -R build ."
