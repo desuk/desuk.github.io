c7dff3a7b2f0768a7a932402e252c63f9cc9e82e
Charlie Dyson <charlie@charliedyson.net>
2020-10-22 20:36:41 +0100

    Better upload script

--- upload.sh
+++ upload.sh
@@ -2,9 +2,13 @@
 
 set -ue -o pipefail
 rm -rf build
-hugo # --cleanDestinationDir
+
+dest=$(mktemp -d)
+trap "rm -rf $dest" EXIT
+
+hugo -d "${dest}"
 host=dukeellington.org.uk
 user=charlied@dukeellington.org.uk
 
 # TODO: This never removes anything :( Should fix this but leave TDWAW alone.
-lftp -c "open $host; mirror -R build ."
+lftp -c "open $host; mirror -R $dest ."
