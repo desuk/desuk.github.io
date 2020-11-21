880dfe59c0e58b2f3d74631018d3bf67bfc6982b
Charlie Dyson <charlie@charliedyson.net>
2020-11-21 17:15:06 +0000

    FIXME FIXME

--- make_preview.sh
+++ make_preview.sh
@@ -2,4 +2,6 @@
 
 set -ue -o pipefail
 
-exec hugo --baseURL=https://filedn.com/lzfSuhW62TUHFogP938o5e8/desuk_preview/ --destination=desuk_preview
+hugo --baseURL=https://filedn.com/lzfSuhW62TUHFogP938o5e8/desuk_preview/ --destination=desuk_preview
+
+rsync -rtv --delete desuk_preview/ "/home/charlie/pCloudDrive/Public Folder/desuk_preview"
